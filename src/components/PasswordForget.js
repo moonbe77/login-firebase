9e2b7a5dd31c0f1856341c56a5eb300c6f10ea01
Berny <moonbe77@gmail.com>
2018-06-27 07:21:30 +1000

    first commit

---
+++ src/components/PasswordForget.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage = () =>
+  <div>
+    <h1>PassForget
+        Page</h1>
+  </div>
+
+export default LandingPage;
---
+++ src/components/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LandingPage = () =>
+  <div>
+    <h1>SignUp Page</h1>
+  </div>
+
+export default LandingPage;
